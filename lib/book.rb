2b0a868daaa1eb82fb9dd5a1b470191bed2e4187
Mike Barberry <mikebarberry@protonmail.com>
2020-10-15 05:10:18 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,13 @@
 class Book
   def initialize(title)
     @title = title
-  end 
+  end
 
   def title=(title)
-    @title = title 
-  end 
+    @title = title
+  end
 
-  def title 
-    @ title 
+  def title
+    @ title
   end 
 end
